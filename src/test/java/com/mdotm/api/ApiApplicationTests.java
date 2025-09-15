278233ede23dcb2b56e486a0d27a449075198160
Germán Furfori <german.furfori@paygoal.com.ar>
2025-09-15 13:29:54 +0200

    First test for get all method, with content

--- src/test/java/com/mdotm/api/ApiApplicationTests.java
+++ src/test/java/com/mdotm/api/ApiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.mdotm.api;
 
-import org.junit.jupiter.api.Test;
+import com.mdotm.api.entity.Pet;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-class ApiApplicationTests {
+@AutoConfigureMockMvc
+@AutoConfigureMockRestServiceServer
+public abstract class ApiApplicationTests {
+	@Autowired
+	protected MockMvc mockMvc;
+	@Autowired
+	protected EntityManager entityManager;
 
-	@Test
-	void contextLoads() {
+	protected void generatePetInDB(String name, String species) {
+		Pet pet = new Pet();
+		pet.setName(name);
+		pet.setSpecies(species);
+
+		entityManager.persist(pet);
+		entityManager.flush();
 	}
-
 }
